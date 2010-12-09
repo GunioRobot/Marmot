a4d0b628e9466bfda14bae88c77a64f1c8c227a3
Dave Hoefler <david.hoefler@punchstock.com>
2010-12-09 17:50:17 -0600

    tab issue

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -6,32 +6,32 @@ app.ajax = {};
 
 
 /*
-	@description
-		A somewhat generic ajax wrapper that takes a 'click_id' and an 'element_id'. Makes an ajax call using the click_id's 'href' param
-		and fills in 'element_id' with the returned data.
+  @description
+    A somewhat generic ajax wrapper that takes a 'click_id' and an 'element_id'. Makes an ajax call using the click_id's 'href' param
+    and fills in 'element_id' with the returned data.
 
-	@args
-		click_id = The element the click event should be attached to
-		element_id = The element that received the data after the ajax call
+  @args
+    click_id = The element the click event should be attached to
+    element_id = The element that received the data after the ajax call
 */
 app.ajax.Link = function(click_id, element_id){
-	$(click_id).click(function(event){   
-		event.preventDefault();
-		$(element_id).empty();
-		var url = $(this).attr('href');
-		$.ajax({
-		  url: url,
-		  success: function(data) {
-				$(element_id).html(data);
-			},
-		  error: function(data) {
-				$(element_id).html(data);
-		  }
-		});
-	});	
+  $(click_id).click(function(event){   
+    event.preventDefault();
+    $(element_id).empty();
+    var url = $(this).attr('href');
+    $.ajax({
+      url: url,
+      success: function(data) {
+        $(element_id).html(data);
+      },
+      error: function(data) {
+        $(element_id).html(data);
+      }
+    });
+  }); 
 };
 
 $(document).ready(function() {
-	// Sets up click events on the artists name using .ajax_link in the sidebar, and fills in the data in the #artist_info element
-	app.ajax.Link('.ajax_link', '#artist_info');
+  // Sets up click events on the artists name using .ajax_link in the sidebar, and fills in the data in the #artist_info element
+  app.ajax.Link('.ajax_link', '#artist_info');
 });
