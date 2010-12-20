45e6e1254b252830aa21ed3aff2a538907ca1b73
Dave Hoefler <david.hoefler@punchstock.com>
2010-12-19 23:21:29 -0600

    Spacing fix

--- server/lib/user.js
+++ server/lib/user.js
@@ -1,28 +1,28 @@
 var CouchClient = require('couch-client'),
-	Users = CouchClient("http://localhost:5984/artist_users"),
-	crypto = require('crypto');
-	
+  Users = CouchClient("http://localhost:5984/artist_users"),
+  crypto = require('crypto');
+  
 function User(auth) {
 
-	function create(req, callback){	  
-		var user_attributes = req.body;
-		var key = auth.encryption_key;
-		var iv = auth.iv;
+  function create(req, callback){   
+    var user_attributes = req.body;
+    var key = auth.encryption_key;
+    var iv = auth.iv;
 
-		var encrypted_password = auth.encrypt(user_attributes.password);
-		
-		var user = {
-			username: user_attributes.username,
-			password: encrypted_password,
-			email_address: user_attributes.email_address,
-			agree_to_terms: user_attributes.agree_to_terms
-		};
-		
-		Users.save(user, function (err, doc) {
-			return callback(doc);
-		});
-		
-	};
+    var encrypted_password = auth.encrypt(user_attributes.password);
+    
+    var user = {
+      username: user_attributes.username,
+      password: encrypted_password,
+      email_address: user_attributes.email_address,
+      agree_to_terms: user_attributes.agree_to_terms
+    };
+    
+    Users.save(user, function (err, doc) {
+      return callback(doc);
+    });
+    
+  };
   
   function exists(id, callback){
     var user_exists = false;
